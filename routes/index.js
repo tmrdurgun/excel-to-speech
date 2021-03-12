759e85e4b5ca832ffdfa813fed17331f43345bf0
Tamer Durgun <tmr.durgun@gmail.com>
2021-03-12 14:23:28 +0300

    list files from files dir

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,23 @@
 var express = require('express');
 var router = express.Router();
+const filesFolder = './files/';
+const fs = require('fs');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express', page: 'Home' });
+router.get('/', (req, res, next) => {
+
+  const files = [];
+
+  fs.readdirSync(filesFolder).forEach(file => {
+    files.push(file);
+  });
+  
+  res.render('index', {
+    page: 'Home',
+    title: 'Welcom to excel to speech App', 
+    desc: 'Upload files and play audio from the file list',
+    files: files 
+  });
 });
 
 module.exports = router;
